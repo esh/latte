55e038ab48891e42ac1aa7600b349ad6b1182e09
esh <chan@sikho.ca>
2009-04-19 00:56:37 +0900

    removing java code from js

---
+++ src/org/latte/scripting/hostobjects/File.java
@@ -0,0 +1,13 @@
+package org.latte.scripting.hostobjects;
+
+import org.mozilla.javascript.ScriptableObject;
+
+
+public class File extends ScriptableObject {
+
+	@Override
+	public String getClassName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
---
+++ src/org/latte/scripting/hostobjects/HostObjectLoader.java
@@ -0,0 +1,9 @@
+package org.latte.scripting.hostobjects;
+
+import org.mozilla.javascript.Scriptable;
+
+public class HostObjectLoader {
+	public static void load(Scriptable scope) {
+		
+	}
+}
