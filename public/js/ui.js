3f0d84ad0eb3f58a8c8ff38d3b740f47b1c83c89
esh <esh@chan-edwards-mac-mini.local>
2009-05-02 19:44:35 +0900

    more ui stuff

--- public/js/ui.js
+++ public/js/ui.js
@@ -5,3 +5,41 @@ function nav(s) {
 function fadeIn(s) {
 	$(s).fadeTo("slow", 1.0)
 }
+
+function loadMore(target, offset) {
+	target.remove();
+	
+	var total = Math.min(offset + 24, thumbs.length) 
+	var html = ""
+	for(var i = offset ; i < total ; i++) {
+		html += "<a href=\"/blog/show/" + type + "/" + thumbs[i] + "\">"
+		html += "<img class=\"hidden\" src=\"/blog/" + thumbs[i] + "/t.jpg\" onload=\"fadeIn(this)\"/>"
+		html += "</a>"
+	}
+	
+	$("#stream").append(html)
+	
+	if(thumbs.length > total) {
+		$("#stream").append("<div id=\"more\">load more</div>")
+		$("#more").click(function() {
+			loadMore($(this), total)
+		})
+	}
+}
+
+function renderThumbs(type, thumbs) {
+	var html = ""
+	for(var i = 0 ; i < Math.min(29, thumbs.length) ; i++) {
+		html += "<a href=\"/blog/show/" + type + "/" + thumbs[i] + "\">"
+		html += "<img src=\"/blog/" + thumbs[i] + "/t.jpg\"/>"
+		html += "</a>"
+	}
+	$("#stream").append(html)
+
+	if(thumbs.length > 29) {
+		$("#stream").append("<div id=\"more\">load more</div>")	
+		$("#more").click(function() {
+			loadMore($(this), 29)
+		})
+	}
+}
