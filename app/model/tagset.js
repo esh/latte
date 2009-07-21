2dc4d563eae2251e358a5a4c0ce52585bd386f79
esh <esh@chan-edwards-mac-mini.local>
2009-07-21 21:33:37 +0900

    force ordering of tags in a set

--- app/model/tagset.js
+++ app/model/tagset.js
@@ -1,7 +1,7 @@
 (function(ds) {
 	function get(tag) {
 		var posts = new Array()
-		ds.query("SELECT post FROM tags WHERE name='" + escape(tag) + "'", function(rs) {
+		ds.query("SELECT post FROM tags WHERE name='" + escape(tag) + "' ORDER BY post ASC", function(rs) {
 			while(rs.next()) posts.push(rs.getInt("post"))
 		})
 		
