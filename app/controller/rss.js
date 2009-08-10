67d5cf11a6a7e73e54e0a4937c969566cd2040ff
esh <esh@chan-edwards-mac-mini.local>
2009-08-10 23:44:25 +0900

    added basic rss support

---
+++ app/controller/rss.js
@@ -0,0 +1,33 @@
+(function() {
+	function show() {
+		require("utils/common.js")
+		const NUM_DISPLAY = 8
+
+		var rss = <rss version="2.0">
+				<channel>
+					<title>Edomame - a photo blog of Ed&apos;s adventures</title>
+					<description>various captioned pictures from Ed</description>
+					<link>http://www.edomame.com</link>
+				</channel>
+			   </rss>
+
+		var keys = model.tagset.get("all")
+		keys.slice(Math.max(0, keys.length - NUM_DISPLAY)).reverse().forEach(function(key) {
+			var post = model.post.get(key)
+			
+			rss.item += <item>
+					<title>{post.title}</title>
+					<description/>
+					<link>http://www.edomame.com/all/{key}</link>
+				    </item>
+		})
+
+
+		return ["ok", "<?xml version=\"1.0\"?>\n" + rss.toXMLString()]
+	}
+	
+	return {
+		show: show
+	}
+})
+
