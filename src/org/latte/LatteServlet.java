cadd32a54d4d8599b325c3645ae6316fe5081a13
Edward Chan <esh@mini-esh.(none)>
2010-02-25 00:58:54 +0900

    fixed serialization in gae

--- src/org/latte/LatteServlet.java
+++ src/org/latte/LatteServlet.java
@@ -4,6 +4,8 @@ import org.latte.scripting.hostobjects.RequestProxy;
 
 import java.io.IOException;
 import java.io.Serializable;
+import java.io.ObjectOutputStream;
+import java.io.ObjectInputStream;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -23,6 +25,8 @@ import org.mozilla.javascript.ContextFactory;
 import org.mozilla.javascript.JavaScriptException;
 import org.mozilla.javascript.Scriptable;
 import org.mozilla.javascript.ScriptableObject;
+import org.mozilla.javascript.serialize.ScriptableOutputStream;
+import org.mozilla.javascript.serialize.ScriptableInputStream;
 
 import org.latte.scripting.Javascript;
 import org.latte.scripting.ScriptLoader;
@@ -32,10 +36,25 @@ public class LatteServlet extends HttpServlet {
 	final private Scriptable parent;
 	private Callable fn;
 
-	private class Session extends ScriptableObject implements Serializable {
-		@Override
-		public String getClassName() {
-			return "Session";
+	private class Session implements Serializable {
+		private Scriptable session;
+	
+		private Session(Context cx) {
+			this.session = cx.newObject(parent);
+		}
+
+		private Scriptable getSession() {
+			return session;
+		}
+
+		private void writeObject(ObjectOutputStream out) throws IOException {
+			ScriptableOutputStream sos = new ScriptableOutputStream(out, parent);
+			sos.writeObject(session);
+		}
+		     
+		private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+			ScriptableInputStream sin = new ScriptableInputStream(in, parent);
+			session = (Scriptable)in.readObject();
 		}
 	}
 
@@ -57,6 +76,7 @@ public class LatteServlet extends HttpServlet {
 		this.parent = parent;
 		this.fn = fn;
 	}
+
 	/**
 	 * 
 	 */
@@ -66,20 +86,17 @@ public class LatteServlet extends HttpServlet {
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {			
 		try {
 			Context cx = ContextFactory.getGlobal().enterContext();
-
-			Session session;
+			Session session = null;
 			if((session = (Session)request.getSession().getAttribute("latte.session")) == null) {
-				session = new Session();
+				session = new Session(cx);
 				request.getSession().setAttribute("latte.session", session);
 			}
-			
-			Scriptable scope = cx.newObject(parent);
-			scope.setParentScope(parent);
+
 			cx.setWrapFactory(new PrimitiveWrapFactory());
-			fn.call(cx, scope, scope, new Object[] {
+			fn.call(cx, parent, parent, new Object[] {
 					new RequestProxy(request),
 					response,
-					session
+					session.getSession()
 			});
 		} catch(Exception e) {
 			LOG.log(Level.SEVERE, "", e);
