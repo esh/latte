bf818d7549cd4ecd0f16714c6d301644b31f76bf
Edward Chan <esh@mini-esh.(none)>
2010-03-03 00:19:12 +0900

    upgrade to newer codec

--- latte.sh
+++ latte.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -classpath lib/latte.jar:lib/jetty-6.1.12rc1.jar:lib/jetty-util-6.1.12rc1.jar:lib/servlet-api-2.5-6.1.12rc1.jar:lib/commons-pool-1.4.jar:lib/commons-beanutils-1.8.0.jar:lib/commons-codec-1.3.jar:lib/js.jar:app org.latte.Run $@
+java -classpath lib/latte.jar:lib/jetty-6.1.12rc1.jar:lib/jetty-util-6.1.12rc1.jar:lib/servlet-api-2.5-6.1.12rc1.jar:lib/commons-pool-1.4.jar:lib/commons-beanutils-1.8.0.jar:lib/commons-codec-1.4.jar:lib/js.jar:app org.latte.Run $@
